95846ce8b13e2eb3ea7feda490ee2f18cf3ef508
Seothp <nikoejento@gmail.com>
2019-10-07 20:48:09 +0300

    add: main page burger menu

---
+++ js/index.js
@@ -0,0 +1,10 @@
+"use strict";
+(function () {
+    const burgerMenu = document.querySelector("#burger-menu");
+    const burgerMenuBtn = burgerMenu.querySelector("#burger-menu__btn");
+    const burgerMenuList = burgerMenu.querySelector("#burger-menu__list");
+
+    burgerMenuBtn.addEventListener("click", () => {
+        burgerMenuList.classList.toggle("active");
+    });
+})();
