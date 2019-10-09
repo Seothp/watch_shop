f872851f662b957c2ba9ca98a450767e26fe2fee
Seothp <nikoejento@gmail.com>
2019-10-09 15:34:29 +0300

    add: card gallery

--- js/index.js
+++ js/index.js
@@ -1,10 +1,22 @@
 "use strict";
 (function () {
     const burgerMenu = document.querySelector("#burger-menu");
-    const burgerMenuBtn = burgerMenu.querySelector("#burger-menu__btn");
+    const burgerMenuBtn = burgerMenu.querySelector("#burger-menu__btn") ? burgerMenu.querySelector("#burger-menu__btn"): "";
     const burgerMenuList = burgerMenu.querySelector("#burger-menu__list");
+
+    const galleryMain = document.querySelector("#gallery__main-img");
+    const galleryList = document.querySelector("#gallery__list");
 
     burgerMenuBtn.addEventListener("click", () => {
         burgerMenuList.classList.toggle("active");
+    });
+    
+
+    galleryList.addEventListener("click", (event)=> {
+        let target = event.target;
+        if (target.tagName === "IMG") {
+            galleryMain.src = target.src;
+            console.log(galleryMain.src);
+        }
     });
 })();
