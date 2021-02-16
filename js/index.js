d6ac8921bbe6f9ab28a4116b39e00ba85bcccb84
Nikolay Geldyev <nikoejento@gmail.com>
2021-02-16 15:54:49 +0300

    fix

--- js/index.js
+++ js/index.js
@@ -1,22 +1,9 @@
 "use strict";
-(function () {
-    const burgerMenu = document.querySelector("#burger-menu");
-    const burgerMenuBtn = burgerMenu.querySelector("#burger-menu__btn") ? burgerMenu.querySelector("#burger-menu__btn"): "";
-    const burgerMenuList = burgerMenu.querySelector("#burger-menu__list");
+const burgerMenu = document.querySelector("#burger-menu");
+const burgerMenuBtn = burgerMenu.querySelector("#burger-menu__btn") ? burgerMenu.querySelector("#burger-menu__btn"): "";
+const burgerMenuList = burgerMenu.querySelector("#burger-menu__list");
 
-    const galleryMain = document.querySelector("#gallery__main-img");
-    const galleryList = document.querySelector("#gallery__list");
+burgerMenuBtn.addEventListener("click", () => {
+    burgerMenuList.classList.toggle("active");
+});
 
-    burgerMenuBtn.addEventListener("click", () => {
-        burgerMenuList.classList.toggle("active");
-    });
-    
-
-    galleryList.addEventListener("click", (event)=> {
-        let target = event.target;
-        if (target.tagName === "IMG") {
-            galleryMain.src = target.src;
-            console.log(galleryMain.src);
-        }
-    });
-})();
